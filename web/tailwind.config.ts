ebd3c3296fdf5aabc4f489bf114caf3d722be5ab
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2023-11-10 11:16:50 -0500

    Remove duplicate tailwind config file

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -2,19 +2,19 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
   },
   plugins: [],
 }
+
 export default config
